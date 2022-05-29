88bbd096490892754a25071763f981d42442f21e
vagrant <vagrant@vagrant.vm>
2022-05-29 08:55:33 +0300

    Update

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -6,6 +6,7 @@ public class Welcomer{
 	}
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
+	}
 
  	public String sayHunter() {
 		return "Test hunter?";
