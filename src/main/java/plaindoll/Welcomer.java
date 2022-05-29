36e01aff328fc7a3c167110ca5e51ddb91bea8ce
vagrant <vagrant@vagrant.vm>
2022-05-29 08:50:14 +0300

    Commit feature/add_reply

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -6,5 +6,8 @@ public class Welcomer{
 	}
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
+
+ 	public String sayHunter() {
+		return "Test hunter?";
 	}
 }
